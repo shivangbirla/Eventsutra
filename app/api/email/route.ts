89fd3ddade3b4816cf16f8b6a00e7c5a23a54d61
shivangbirla <birlashivang01@gmail.com>
2024-03-23 17:05:23 +0530

    changes

--- app/api/email/route.ts
+++ app/api/email/route.ts
@@ -1,38 +1,41 @@
-// import { Resend } from "resend";
-// import { NextResponse } from "next/server";
-// import WelcomeEmail from "../../../components/shared/Welcome";
+import { Resend } from "resend";
+import { NextResponse } from "next/server";
+import WelcomeEmail from "../../../components/shared/Welcome";
 
 // const resend = new Resend(process.env.RESEND_API_KEY);
 
-// export async function POST(request: Request) {
-//   const { firstName, email, qrUrl } = await request.json();
-//   // try {
-//   //   await resend.emails.send({
-//   //     from: "onboarding@resend.dev",
-//   //     to: email,
-//   //     subject: "Welcome to Resend!",
-//   //     react: WelcomeEmail({
-//   //       userFirstName: firstName,
-//   //       loginDate: new Date(2023, 0, 15, 14, 30),
-//   //       loginDevice: "Mobile",
-//   //       loginLocation: "San Francisco",
-//   //       loginIp: "203.0.113.1",
-//   //       qrUrl: qrUrl,
-//   //     }),
-//   //   });
+export async function POST(request: Request) {
+  const { firstName, email, qrUrl } = await request.json();
+  // try {
+  //   await resend.emails.send({
+  //     from: "onboarding@resend.dev",
+  //     to: email,
+  //     subject: "Welcome to Resend!",
+  //     react: WelcomeEmail({
+  //       userFirstName: firstName,
+  //       loginDate: new Date(2023, 0, 15, 14, 30),
+  //       loginDevice: "Mobile",
+  //       loginLocation: "San Francisco",
+  //       loginIp: "203.0.113.1",
+  //       qrUrl: qrUrl,
+  //     }),
+  //   });
 
-//   //   return NextResponse.json({ status: "ok" }, { status: 200 });
-//   // } catch (e: unknown) {
-//   //   if (e instanceof Error) {
-//   //     console.log(`Failed to send email: ${e.message}`);
-//   //   }
-//   //   return NextResponse.json(
-//   //     {
-//   //       error: "Internal server error.",
-//   //     },
-//   //     {
-//   //       status: 500,
-//   //     }
-//   //   );
-//   // }
-// }
+  //   // Await the NextResponse.json() call to ensure proper response handling
+  //   return await NextResponse.json({ status: "ok" }, { status: 200 });
+  // } catch (error) {
+  //   if (error instanceof Error) {
+  //     console.error(`Failed to send email: ${error.message}`);
+  //     return await NextResponse.json(
+  //       { error: "Failed to send email. Please try again later." },
+  //       { status: 500 }
+  //     );
+  //   }
+  //   // Handle unknown error types here
+  //   console.error("Unknown error occurred:", error);
+  //   return await NextResponse.json(
+  //     { error: "Internal server error." },
+  //     { status: 500 }
+  //   );
+  // }
+}
