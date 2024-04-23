81f7140059e814b69921eb908f9ada7c9da41c1d
shivangbirla <birlashivang01@gmail.com>
2024-04-24 02:56:00 +0530

    changesss

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -40,7 +40,7 @@ const EventDetails = async ({
               <div className="flex flex-col gap-3 sm:flex-row sm:items-center">
                 <div className="flex gap-3">
                   <p className="p-bold-20 rounded-full bg-green-500/10 dark:bg-[#155d27] dark:text-white px-5 py-2 text-green-700">
-                    {event.isFree ? "FREE" : `$${event.price}`}
+                    {event.isFree ? "FREE" : `₹ ${event.price}`}
                   </p>
                   <p className="p-medium-16 rounded-full bg-grey-500/10 px-4 py-2.5 text-grey-500 dark:bg-slate-500 dark:text-white">
                     {event.category.name}
